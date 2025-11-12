4f4137880f43f38818a4bdc55cdba45690c3e1f0
tgirgash <tomgirgashjr@gmail.com>
2025-11-12 15:45:14 -0500

    created EventSummary model replacing Ticketmaster event class

---
+++ Where2Play/Models/EventSummary.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Where2Play.Models
+{
+    /// <summary>
+    /// Combines artist and concert information from MusicBrainz and Setlist.fm.
+    public class EventSummary
+    {
+        // Artist info (MusicBrainz)
+        public string ArtistName { get; set; }
+        public string Genre { get; set; }
+        public string Country { get; set; }
+
+        // Concert info (Setlist.fm)
+        public string Venue { get; set; }
+        public string City { get; set; }
+        public DateTime? Date { get; set; }
+        public string Url { get; set; }
+
+       
+    }
+}
